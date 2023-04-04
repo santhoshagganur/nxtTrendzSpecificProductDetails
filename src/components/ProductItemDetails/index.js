1b911dceb6746d0e3b5f77e9a2ee01eef7a50e5e
santhoshagganur <agganursanthosh@gmail.com>
2023-04-04 15:17:46 +0000

    last route

--- src/components/ProductItemDetails/index.js
+++ src/components/ProductItemDetails/index.js
@@ -1,6 +1,8 @@
 // Write your code here
 import {Component} from 'react'
-import {Cookies} from 'js-cookie'
+import {BsDashSquare, BsPlusSquare} from 'react-icons/bs'
+
+import Cookies from 'js-cookie'
 import Header from '../Header'
 import './index.css'
 
@@ -16,6 +18,7 @@ class ProductItemDetails extends Component {
     productData: {},
     similarProducts: [],
     apiStatus: apiStatusConstants.initial,
+    quantity: 1,
   }
 
   componentDidMount() {
@@ -30,6 +33,8 @@ class ProductItemDetails extends Component {
     totalReviews: data.total_reviews,
     rating: data.rating,
     availability: data.availability,
+    price: data.price,
+    description: data.description,
   })
 
   getDetails = async () => {
@@ -37,7 +42,6 @@ class ProductItemDetails extends Component {
     const {params} = match
     const {id} = params
     const jwtToken = Cookies.get('jwt_token')
-    const url = `http://localhost:3000/products/${id}`
     const options = {
       method: 'GET',
       headers: {
@@ -45,7 +49,7 @@ class ProductItemDetails extends Component {
       },
     }
 
-    const response = await fetch(url, options)
+    const response = await fetch(`https://apis.ccbp.in/products/${id}`, options)
     console.log(response)
 
     if (response.ok) {
@@ -71,14 +75,55 @@ class ProductItemDetails extends Component {
 
   renderSuccessView = () => {
     console.log('yes')
-    const {productData, similarProducts} = this.state
-    const {imageUrl, title} = productData
+    const {productData, similarProducts, quantity} = this.state
+    const {
+      imageUrl,
+      title,
+      price,
+      description,
+      rating,
+      totalReviews,
+      availability,
+      brand,
+    } = productData
 
     return (
       <div className="product-card-container">
         <img src={imageUrl} alt="product" className="product-image" />
         <div className="product-details-container">
           <h1 className="product-name"> {title} </h1>
+          <p className="product-price"> RS {price}/- </p>
+          <div className="rating-holder-container">
+            <div className="rating-box">
+              <p className="user-ratings"> {rating} </p>
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+                alt="star"
+                className="rating-image"
+              />
+            </div>
+            <p> {totalReviews} Reviews </p>
+          </div>
+          <p className="product-description"> {description} </p>
+          <p className="special-elements">
+            <span className="span-element"> Available: </span> {availability}
+          </p>
+          <p className="special-elements">
+            <span className="span-element"> Brand: </span> {brand}
+          </p>
+          <hr className="horizontal-line" />
+          <div className="quantity-container">
+            <button className="quantity-buttons" type="button">
+              <BsDashSquare className="user-interaction-button" />
+            </button>
+            <p className="quantity"> {quantity} </p>
+            <button className="quantity-buttons" type="button">
+              <BsPlusSquare className="user-interaction-button" />
+            </button>
+          </div>
+          <button type="button" className="add-to-cart-btn">
+            ADD TO CART
+          </button>
         </div>
       </div>
     )
@@ -101,10 +146,12 @@ class ProductItemDetails extends Component {
 
   render() {
     return (
-      <div className="product-item-details-container">
+      <>
         <Header />
-        {this.renderProductsDetails()}
-      </div>
+        <div className="product-item-details-container">
+          {this.renderProductsDetails()}
+        </div>
+      </>
     )
   }
 }
