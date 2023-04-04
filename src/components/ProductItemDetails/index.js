5f9ee29c876b44a5247e15a9d7b266ce03b9269d
santhoshagganur <agganursanthosh@gmail.com>
2023-04-04 17:20:25 +0000

    test cases

--- src/components/ProductItemDetails/index.js
+++ src/components/ProductItemDetails/index.js
@@ -1,8 +1,10 @@
 // Write your code here
 import {Component} from 'react'
+import {Loader} from 'react-loader-spinner'
 import {BsDashSquare, BsPlusSquare} from 'react-icons/bs'
 
 import Cookies from 'js-cookie'
+import SimilarProductItem from '../SimilarProductItem'
 import Header from '../Header'
 import './index.css'
 
@@ -23,6 +25,7 @@ class ProductItemDetails extends Component {
 
   componentDidMount() {
     this.getDetails()
+    this.setState({apiStatus: apiStatusConstants.inProgress})
   }
 
   getFormattedData = data => ({
@@ -73,6 +76,18 @@ class ProductItemDetails extends Component {
     }
   }
 
+  onDecrement = () => {
+    this.setState(prevState => ({
+      quantity: prevState.quantity - 1,
+    }))
+  }
+
+  onIncrement = () => {
+    this.setState(prevState => ({
+      quantity: prevState.quantity + 1,
+    }))
+  }
+
   renderSuccessView = () => {
     console.log('yes')
     const {productData, similarProducts, quantity} = this.state
@@ -88,44 +103,60 @@ class ProductItemDetails extends Component {
     } = productData
 
     return (
-      <div className="product-card-container">
-        <img src={imageUrl} alt="product" className="product-image" />
-        <div className="product-details-container">
-          <h1 className="product-name"> {title} </h1>
-          <p className="product-price"> RS {price}/- </p>
-          <div className="rating-holder-container">
-            <div className="rating-box">
-              <p className="user-ratings"> {rating} </p>
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-                alt="star"
-                className="rating-image"
-              />
+      <>
+        <div className="product-card-container">
+          <img src={imageUrl} alt="product" className="product-image" />
+          <div className="product-details-container">
+            <h1 className="product-name"> {title} </h1>
+            <p className="product-price"> RS {price}/- </p>
+            <div className="rating-holder-container">
+              <div className="rating-box">
+                <p className="user-ratings"> {rating} </p>
+                <img
+                  src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+                  alt="star"
+                  className="rating-image"
+                />
+              </div>
+              <p> {totalReviews} Reviews </p>
             </div>
-            <p> {totalReviews} Reviews </p>
-          </div>
-          <p className="product-description"> {description} </p>
-          <p className="special-elements">
-            <span className="span-element"> Available: </span> {availability}
-          </p>
-          <p className="special-elements">
-            <span className="span-element"> Brand: </span> {brand}
-          </p>
-          <hr className="horizontal-line" />
-          <div className="quantity-container">
-            <button className="quantity-buttons" type="button">
-              <BsDashSquare className="user-interaction-button" />
+            <p className="product-description"> {description} </p>
+            <p className="special-elements">
+              <span className="span-element"> Available: </span> {availability}
+            </p>
+            <p className="special-elements">
+              <span className="span-element"> Brand: </span> {brand}
+            </p>
+            <hr className="horizontal-line" />
+            <div className="quantity-container">
+              <button
+                className="quantity-buttons"
+                type="button"
+                onClick={this.onDecrement}
+              >
+                <BsDashSquare className="user-interaction-button" />
+              </button>
+              <p className="quantity"> {quantity} </p>
+              <button
+                className="quantity-buttons"
+                type="button"
+                onClick={this.onIncrement}
+              >
+                <BsPlusSquare className="user-interaction-button" />
+              </button>
+            </div>
+            <button type="button" className="add-to-cart-btn">
+              ADD TO CART
             </button>
-            <p className="quantity"> {quantity} </p>
-            <button className="quantity-buttons" type="button">
-              <BsPlusSquare className="user-interaction-button" />
-            </button>
           </div>
-          <button type="button" className="add-to-cart-btn">
-            ADD TO CART
-          </button>
         </div>
-      </div>
+        <h1> Similar Products </h1>
+        <ul className="similar-products-container">
+          {similarProducts.map(each => (
+            <SimilarProductItem details={each} key={each.id} />
+          ))}
+        </ul>
+      </>
     )
   }
 
--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -1,1 +1,28 @@
 // Write your code here
+import './index.css'
+
+const SimilarProductItem = props => {
+  const {details} = props
+  const {title, imageUrl, brand, price, rating} = details
+
+  return (
+    <li className="similar-product">
+      <img src={imageUrl} alt="similar" className="similar-product-image" />
+      <h1> {title} </h1>
+      <p> by {brand} </p>
+      <div className="price-rating-container">
+        <p> Rs {price}/- </p>
+        <div className="rating-box">
+          <p className="user-ratings"> {rating} </p>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+            alt="star"
+            className="rating-image"
+          />
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default SimilarProductItem
